22562a7737b4f4ca873f4621608c0b2cdcaa6f95
Tanguy-Supinfo <tanguy.joseph@supinfo.com>
2021-01-15 12:56:26 +0100

    origin master

---
+++ src/java/fr/oneocass/back/HomeBeans.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.oneocass.back;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Tanguy
+ */
+@ManagedBean
+@RequestScoped
+public class HomeBeans {
+
+    /**
+     * Creates a new instance of HomeBeans
+     */
+    public HomeBeans() {
+    }
+    
+}
